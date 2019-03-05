1be2dfb4225eff2d21f96220693abf86429b465e
Alexander V. Smal <avsmal@gmail.com>
2019-03-05 16:57:35 +0300

    Сборка результатов по выборам губернатора и по выборам в МО

---
+++ compose-results.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+for prefix in mo gub
+do
+	rm -f ${prefix}.csv
+	for f in ${prefix}/*.dat
+	do 
+		cat $f >> ${prefix}.csv
+	done
+done
+
+FIELDS=5
+
+rm -f movsgub.csv
+for f in mo/*.dat
+do 
+	fn=${f#mo/}
+	uik=${fn#uik-}
+	uik=${uik%.dat} 
+	DATAMO=`cut -f1-15 -d, mo/${fn}` 
+	DATAGU=`cut -f2-15 -d, gub/${fn}` 
+	echo "$DATAMO,$DATAGU" >> movsgub.csv
+done
+
